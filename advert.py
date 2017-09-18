b32df972fd451abb6f8ac899a3f3a4f362ecc3a4
Andrew Zyabin <schas002@users.noreply.github.com>
2017-09-18 14:27:15 +0400

    :sparkles: implement multiple advertfiles

--- advert.py
+++ advert.py
@@ -1,8 +1,15 @@
-import random
+#!/usr/bin/env python3
+# To use, either
+#  1. name any of the advertfiles `advertfile` and run advert.py without additional arguments
+#  2. pass the name of the advertfile you want to use as the first additional argument to advert.py
+
+import sys, random
+
+advertfile = sys.argv[1] if len(sys.argv) > 1 else 'advertfile'
 
 ads = []
 
-with open('advertfile', 'r') as f:
+with open(advertfile, 'r') as f:
     ads = f.read().split('\n%\n')
 
 while True:
